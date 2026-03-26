73d955dcb44b04fa5e7d1310407715918e3cdcf4
RamonGbit <ramonomezflorez@gmail.com>
2026-03-26 01:32:38 -0400

    feat: created the struct storage, used serde, fs and io for the Json management, writing and reading the archives and error management, created the constructor for the storage object and two functions, save_tasks and read_tasks

--- src/storage.rs
+++ src/storage.rs
@@ -19,4 +19,28 @@ impl Storage {
             file_path: file_path.to_string(),
         }
     }
+    /// funcion para guardar las tareas en el archivo Json
+    pub fn save_tasks(&self, tasks: &Vec<Task>) -> io::Result<()> {
+        /// convertimos el vector de tareas en un string Json mediante serde
+        /// si hay un error lo convertimos a un error de io
+        let json = serde_json::to_string_pretty(tasks)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        /// escribimos en el archivo usando la ruta guardada
+        fs::write(&self.file_path, json)?;
+        /// retornamos Ok(()) para indicar que todo salio bien
+        Ok(())
+    }
+    pub fn read_tasks(&self) -> io::Result<Vec<Task>> {
+        /// verificamos la existencia del archivo
+        if !std::path::Path::new(&self.file_path).exists() {
+            return Ok(Vec::new());
+        }
+        /// leemos el archivo
+        let archive_content = fs::read_to_string(&self.file_path)?;
+        /// usamos serde para convertir el string Json en un vector
+        /// si hay un error lo convertimos a un error de io
+        let tasks = serde_json::from_str(&archive_content)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        Ok(tasks)
+    }
 }
