8030196f6fe3ba4ab73c9c1709dd84cbdaafab47
RamonGbit <ramonomezflorez@gmail.com>
2026-03-26 11:06:39 -0400

    feat: added a secure password system for users

--- src/users.rs
+++ src/users.rs
@@ -1,14 +1,26 @@
+/// invocamos bcrypt para el manejo de las contraseñas
+/// invocamos serde para el manejo y creacion de los Json
+use bcrypt::{hash, verify, DEFAULT_COST};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+/// creamos la estructura User que se encarga de manejar los usuarios
 pub struct User {
+    // definimos atributos
     id: u32,
     username: String,
+    password_hash: String,
 }
 
 impl User {
-    pub fn new(id: u32, username: String) -> Self {
-        Self { id, username }
+    // constructor del objeto User
+    pub fn new(id: u32, username: String, password: &str) -> Result<Self, bcrypt::BcryptError> {
+        let password_hash = hash(password, DEFAULT_COST)?;
+        Ok(Self {
+            id,
+            username,
+            password_hash,
+        })
     }
 
     // Getters
@@ -18,5 +30,9 @@ impl User {
 
     pub fn username(&self) -> &str {
         &self.username
+    }
+
+    pub fn verify_password(&self, password: &str) -> bool {
+        verify(password, &self.password_hash).unwrap_or(false)
     }
 }
